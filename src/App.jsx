d0c3be431edbd985affb6ead4197547dd224a86e
pawan-jagdamb <pawankumar9534078@gmail.com>
2025-03-15 11:44:25 +0530

    update password name changed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Signup from './pages/Signup'
 import Login from './pages/Login'
 import  {OpenRoute} from "/src/components/core/Auth/OpenRoute"
 import { ForgotPassword } from './pages/ForgotPassword'
-import { UpdatePassword } from './pages/updatePassword'
+import { UpdatePassword } from './pages/UpdatePassword'
 import { VerifyEmail } from './pages/VerifyEmail'
 import { About } from './pages/About'
 import { Contact } from './pages/Contact'
